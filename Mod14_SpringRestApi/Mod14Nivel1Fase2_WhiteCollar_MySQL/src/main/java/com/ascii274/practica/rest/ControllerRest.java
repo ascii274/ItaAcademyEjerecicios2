c0a666c0ae1c210984cc6728939195500023c163
Joel Cabatay <asci274@gmail.com>
2021-10-26 19:01:23 +0200

    Mod14 Nivell 1 Fase2. Initial Commit.

---
+++ Mod14_SpringRestApi/Mod14Nivel1Fase2_WhiteCollar_MySQL/src/main/java/com/ascii274/practica/rest/ControllerRest.java
@@ -0,0 +1,26 @@
+package com.ascii274.practica.rest;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ascii274.practica.dao.BotiguesDAO;
+import com.ascii274.practica.dao.QuadresDAO;
+import com.ascii274.practica.models.Botiga;
+import com.ascii274.practica.models.Quadre;
+
+@RestController
+@RequestMapping("/whitecollar")
+
+public class ControllerRest {
+	
+	
+}
